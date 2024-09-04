2b04012e866da9f70aa935e21dbefdad66af4ca0
osdanova <osdanovaonline@outlook.com>
2024-09-05 00:20:25 +0200

    Exporter fixes

--- ModelingToolkit.Formats/MtPorter.cs
+++ ModelingToolkit.Formats/MtPorter.cs
@@ -9,7 +9,7 @@ namespace ModelingToolkit.Formats
         public static MtScene ImportScene(string filepath)
         {
             string extension = Path.GetExtension(filepath).ToLower();
-            if(extension == ".fbx")
+            if (extension == ".fbx")
             {
                 return MtAssimp.AssimpImporter.ImportScene(filepath);
             }
@@ -31,13 +31,14 @@ namespace ModelingToolkit.Formats
                 Assimp.Scene assimpScene = MtAssimp.AssimpExporter.ExportScene(scene);
                 Assimp.AssimpContext context = new Assimp.AssimpContext();
                 context.ExportFile(assimpScene, filepath, "fbx");
+                ExportTextures(scene, filepath);
             }
-            else if (fileType == FileType.GLB)
+            else if (fileType == FileType.GLTF)
             {
                 SceneBuilder sb = MtGltf.GltfExporter.CreateScene(scene);
                 sb.ToGltf2().SaveGLTF(filepath);
             }
-            else if (fileType == FileType.GLTF)
+            else if (fileType == FileType.GLB)
             {
                 SceneBuilder sb = MtGltf.GltfExporter.CreateScene(scene);
                 sb.ToGltf2().SaveGLB(filepath);
@@ -59,9 +60,20 @@ namespace ModelingToolkit.Formats
         }
 
         /*
+         * Exports the textures from the given scene to the given model filepath
+         */
+        public static void ExportTextures(MtScene scene, string filePath, string prefix = "")
+        {
+            foreach (MtScene.MtModelNode modelNode in scene.Models)
+            {
+                ExportTextures(modelNode.Model, filePath, prefix);
+            }
+        }
+
+        /*
          * Exports the textures from the given model to the given model filepath
          */
-        public void ExportTextures(string filePath, MtModel model, string prefix = "")
+        public static void ExportTextures(MtModel model, string filePath, string prefix = "")
         {
             for (int i = 0; i < model.Materials.Count; i++)
             {
